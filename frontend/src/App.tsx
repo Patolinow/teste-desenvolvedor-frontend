c6e31a3288f8f038d913c8fd44d4c68dae3d6f5d
Patolinow <fabiodasn1@gmail.com>
2024-04-28 02:00:59 -0300

    [feat]: adicionado sistema de rotas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,22 @@
-import './App.scss'
+import "./App.scss";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./pages/Root";
+import BularioPage from "./pages/BularioPage";
+import SearchPage, { searchLoader } from "./pages/SearchPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { index: true, element: <BularioPage /> },
+      { path: "search", element: <SearchPage />, loader: searchLoader },
+    ],
+  },
+]);
 
 function App() {
-
-
-  return (
-    <>
-    
-    </>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
